8f6d42ce09f9b28b47a782feba5f41ced0814051
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-07-14 23:35:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
+    if
     (@number1+@number2).to_s
     (@number1-@number2).to_s
     (@number1*@number2).to_s
